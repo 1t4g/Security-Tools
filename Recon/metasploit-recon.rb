f2281e3e6399230a40e48545eccd2c41c2c02457
Peter Bassill <peter.bassill@hedgehogsecurity.co.uk>
2015-07-25 05:47:22 +0100

    Update metasploit-recon.rb

--- Recon/metasploit-recon.rb
+++ Recon/metasploit-recon.rb
@@ -1,1 +1,11 @@
 ;;;
+
+RHOSTS='192.168.33.200-254'
+
+db_nmap -sS -vvv -O $RHOSTS
+use auxiliary/scanner/ftp/
+run
+use auxiliary/scanner/ssh/ssh_version
+run
+use auxiliary/scanner/rdp/ms12_020_check
+run
