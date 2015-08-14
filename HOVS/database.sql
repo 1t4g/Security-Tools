e6397334a64e75f0ac50fa28568c0a368b40498b
Peter Bassill <peter.bassill@hedgehogsecurity.co.uk>
2015-08-14 11:01:01 +0100

    Update database.sql

--- HOVS/database.sql
+++ HOVS/database.sql
@@ -6,3 +6,15 @@ fname varchar(100),
 sname varchar(100),
 password varchar(255)
 );
+
+
+CREATE TABLE scans (
+scanid INT AUTO INCREMENT NOT_NULL,
+name varchar(255),
+email varchar(255),
+phone varchar(255),
+company varchar(255),
+number varchar(255),
+target varchar(255),
+PRIMARY KEY (scanid)
+);
